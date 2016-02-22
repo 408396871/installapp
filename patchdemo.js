fad76a10e37fb3cea73a898cda14e9719c2e1c2e
jdong <408396871@qq.com>
2016-02-22 14:49:14 +0800

    4
    4

--- patchdemo.js
+++ patchdemo.js
@@ -4,7 +4,7 @@ var fileerr;
 
 var url = NSURL.fileURLWithPath(filePath);
 var data = NSData.alloc().initWithContentsOfURL(url);
-var dic = NSJSONSerialization.JSONObjectWithData_options_error(data, NSJSONReadingMutableLeaves, fileerr);
+var dic = NSJSONSerialization.JSONObjectWithData_options_error(data, NSJSONReadingMutableLeaves,fileerr);
 if (fileerr != null) {
     console.log("URL路径文件获取失败--------:%");
     return null;
