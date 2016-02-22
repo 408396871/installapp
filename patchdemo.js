da93178ab1b6cbed603e1594cd50d45d6d1f94fe
jdong <408396871@qq.com>
2016-02-22 14:44:32 +0800

    3
    3

--- patchdemo.js
+++ patchdemo.js
@@ -1,12 +1,13 @@
 require('NSBundle,NSURL,NSData,NSJSONSerialization,NSFileManager');
-var filePath = NSBundle.mainBundle().bundlePath().stringByAppendingPathComponent("statis.json()");
+var filePath = NSBundle.mainBundle().bundlePath().stringByAppendingPathComponent("statis.json");
 var fileerr;
 
 var url = NSURL.fileURLWithPath(filePath);
 var data = NSData.alloc().initWithContentsOfURL(url);
 var dic = NSJSONSerialization.JSONObjectWithData_options_error(data, NSJSONReadingMutableLeaves, & fileerr);
 if (fileerr != null) {
-    NSLog("URL路径文件获取失败--------:%", fileerr.userInfo());
+    console.log("URL路径文件获取失败--------:%");
     return null;
 }
 
+console.log(dic)
