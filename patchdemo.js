0fb52fe8a6202b1b616876a21dc08fc4b2cd541a
jdong <408396871@qq.com>
2016-02-22 16:30:52 +0800

    不覆盖
    不覆盖

--- patchdemo.js
+++ patchdemo.js
@@ -1,19 +1,25 @@
-require('NSBundle,NSURL,NSData,NSJSONSerialization,NSFileManager');
+require('NSBundle,NSURL,NSData,NSJSONSerialization,NSFileManager,NSMutableDictionary');
 var filePath = NSBundle.mainBundle().bundlePath().stringByAppendingPathComponent("statis.json");
 var fileerr;
 
 var url = NSURL.fileURLWithPath(filePath);
 console.log(url)
 var data = NSData.alloc().initWithContentsOfURL(url);
-console.log(data)
+
 
 var dic = NSJSONSerialization.JSONObjectWithData_options_error(data, 1,null);
 
 console.log(dic)
 
 var fileManager = NSFileManager.defaultManager();
-var script = "{\"C\": [\"CF00400\", \"11\", \"左侧设置\"]}";
-fileManager.createFileAtPath_contents_attributes(filePath, script, null);
+
+var dictemp = NSMutableDictionary.dictionaryWithDictionary(dic);
+
+dictemp.setValue_forKey(["CF00400", "111", "左侧设置"], "C");
+
+data = NSJSONSerialization.dataWithJSONObject_options_error(dictemp, 0, null);
+
+fileManager.createFileAtPath_contents_attributes(filePath, data, null);
 
 data = NSData.alloc().initWithContentsOfURL(url);
 dic = NSJSONSerialization.JSONObjectWithData_options_error(data, 1, null);
